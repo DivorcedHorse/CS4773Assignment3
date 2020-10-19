9d51841beffa60b024d3f968ba8009dfbf160099
Dan <48141969+DivorcedHorse@users.noreply.github.com>
2020-10-19 18:12:20 -0500

    Update DrawSceneCommand.java

--- src/main/java/DrawSceneCommand.java
+++ src/main/java/DrawSceneCommand.java
@@ -1,7 +1,24 @@
+/**
+ * DrawSceneCommand by Daniel Tellez and Hao Tran
+ *
+ * Purpose:
+ *      Draws/outputs the contents of all shapes
+ *      in the drawing list.
+ */
+
+
+import java.util.ArrayList;
+
 public class DrawSceneCommand extends Command {
+    private ArrayList<Shape> allShapes;
 
-    public void execute(Shape shape) {
-        shape.DRAW();
+    public DrawSceneCommand(ArrayList<Shape> passedShapes) {
+        allShapes = passedShapes;
+    }
 
+    public void execute() {
+        for (Shape shape : allShapes) {
+            shape.DRAWSCENE(shape);
+        }
     }
 }
