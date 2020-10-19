4da873f4e86fbdc813d27b08e5b21b2df65fda22
Dan <48141969+DivorcedHorse@users.noreply.github.com>
2020-10-19 18:12:06 -0500

    Update DrawCommand.java

--- src/main/java/DrawCommand.java
+++ src/main/java/DrawCommand.java
@@ -1,6 +1,19 @@
+/**
+ * DrawCommand by Daniel Tellez and Hao Tran
+ *
+ * Purpose:
+ *      Command class for outputting the shapes contents.
+ */
+
 public class DrawCommand extends Command {
-    public void execute(Shape shape) {
-        shape.DRAW();
+    Shape myShape;
+
+    public DrawCommand(Shape shape) {
+        myShape = shape;
+    }
+
+    public void execute() {
+        myShape.DRAW();
 
     }
 }
