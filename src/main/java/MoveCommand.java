082e696eb45f76cb0a9fdfe59f11ecf66ad428a7
Dan <48141969+DivorcedHorse@users.noreply.github.com>
2020-10-19 18:12:33 -0500

    Update MoveCommand.java

--- src/main/java/MoveCommand.java
+++ src/main/java/MoveCommand.java
@@ -1,13 +1,24 @@
-public class MoveCommand extends Command {
-    int xCord, yCord;
-    public MoveCommand(int x, int y){
-        xCord = x;
-        yCord = y;
+/**
+ * MoveCommand by Daniel Tellez and Hao Tran
+ *
+ * Purpose:
+ *      Command class for moving the coordinates/points
+ *      of a shape.
+ */
 
+public class MoveCommand extends Command {
+    private Shape myShape;
+    private int xCord;
+    private int yCord;
+
+    public MoveCommand(Shape shape, int newXLocation, int newYLocation) {
+        myShape = shape;
+        xCord = newXLocation;
+        yCord = newYLocation;
     }
 
-    public void execute(Shape shape) {
-        shape.MOVE(xCord, yCord);
-
+    public void execute() {
+        myShape.MOVE(xCord, yCord);
     }
 }
+
