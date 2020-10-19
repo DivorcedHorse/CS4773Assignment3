10f0bb0e188aa10376cf38ff2ee1beb6b879d38a
Dan <48141969+DivorcedHorse@users.noreply.github.com>
2020-10-19 18:11:51 -0500

    Update DeleteCommand.java

--- src/main/java/DeleteCommand.java
+++ src/main/java/DeleteCommand.java
@@ -1,5 +1,20 @@
-public class DeleteCommand extends Command {
-    public void execute(Shape shape) {
+/**
+ * DeleteCommand by Daniel Tellez and Hao Tran
+ *
+ * Purpose:
+ *      Command class for deleting a shape.
+ */
 
+
+public class DeleteCommand extends Command {
+    Shape myShape;
+
+    public DeleteCommand(Shape shape) {
+        myShape = shape;
+    }
+
+    public void execute() {
+
+        myShape.DELETE();
     }
 }
